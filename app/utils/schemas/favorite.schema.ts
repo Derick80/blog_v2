770b6f4573c92d366639194ef23e2824e1a9774c
Derick80 <derickchoskinson@gmail.com>
2023-03-23 23:16:39 -0500

    fixed types

--- app/utils/schemas/favorite.schema.ts
+++ app/utils/schemas/favorite.schema.ts
@@ -3,7 +3,5 @@ import type { SerializeFrom } from '@remix-run/node'
 import type { Post, SerializedPost } from './post-schema'
 
 export type Favorite = SerializeFrom<PrismaFavorites> & {
-  post: {
-    data: SerializedPost
-  }
+  post: Post
 }
