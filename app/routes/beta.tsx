a046bdf618cd4ffd2745d98605fdd3d49bf6c4bb
Derick80 <derickchoskinson@gmail.com>
2023-03-24 12:10:31 -0500

    minor css experiments in beta

--- app/routes/beta.tsx
+++ app/routes/beta.tsx
@@ -1,4 +1,4 @@
-import { StarFilledIcon, StarIcon } from '@radix-ui/react-icons'
+import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon, Cross1Icon, StarFilledIcon, StarIcon } from '@radix-ui/react-icons'
 import { json } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 import React from 'react'
@@ -17,24 +17,29 @@ export async function loader({ request }: { request: Request }) {
 export default function BetaRoute() {
   const data = useLoaderData()
   const [open, setOpen] = React.useState(false)
+
   return (
     <div className='relative grid grid-cols-12 max-w-2xl mx-auto h-screen'>
 
-        {/* <div
-          className='hidden relative md:flex md:col-span-2 col-start-1 h-sfull bg-blue-500 w-full' > */}
+
 {
   open && (
-    <div className='absolute top-0 left-0 w-[250px] h-full bg-crimson8'>
-      stuf headers
+    <div className='absolute w-[250px] h-full bg-crimson8'>
+     <button className={ `absolute right-0 z-10 p-2 rounded-full bg-crimson8` }
+        onClick={ () => setOpen(!open) }
+      >
+        <Cross1Icon />
+      </button>
     </div>
   )
 }
-      <button className='absolute right-0 md:left-auto flex  text-crimson8'
+      <button className={ `absolute right-0 z-10 p-2 rounded-full bg-crimson8` }
         onClick={ () => setOpen(!open) }
       >
-        <StarIcon />
+        {
+          open ? <ChevronLeftIcon /> : <ChevronDownIcon />
+        }
       </button>
-        {/* </div> */}
         <div className='col-span-12 md:col-span-12 flex col-stsart-3 w-fsull bg-purple-500 h-scrseen'>
 
 
