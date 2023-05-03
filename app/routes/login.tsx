b29dfcc2dede94ada8f46e22755e2b5f27812fb9
Derick80 <derickchoskinson@gmail.com>
2023-05-03 15:41:42 -0500

    fix

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -35,7 +35,7 @@ export default function Login() {
       <div className='flex items-center justify-center gap-2'>
         <SocialLoginForm provider='discord'>
           <Button
-            
+            // fix
             variant='icon_unfilled'
             size='small'
           className='flex flex-col items-center gap-2'>
